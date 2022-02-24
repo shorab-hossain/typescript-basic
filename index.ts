a3b1e77e7cac2bced7ba5a95781b108cb88eee73
shorab hossain <mdshakiil720516@gmail.com>
2022-02-24 20:42:38 +0600

    vs code theme

---
+++ index.ts
@@ -0,0 +1,51 @@
+// const characters = 'shorab';
+// console.log(characters);
+// const inputs = document.querySelectorAll('input');
+// console.log(inputs);
+// inputs.forEach(input => {
+//       console.log(input)
+// });
+// let first = 'mario';
+// let age = 30;
+// let isBlackBelt = false;
+// first ='habib';
+// age = 30;
+// const circ = (diameter:number) =>{
+//       return diameter * Math.PI
+// }
+// console.log(circ(7.5))
+// let  character:string;
+// let age:number;
+// let loggedIn:boolean;
+// //character
+// character = 'shorab';
+// //age 
+// age  = 30;
+// //loggedIn
+// loggedIn = false;
+// //union types
+// let mixed:(string | number | boolean)[]=[];
+// mixed.push('habib');
+// mixed.push(20);
+// mixed.push(true)
+// console.log(mixed);
+// let ninjaOne:object;
+// ninjaOne = {name:'shakil',age:20};
+// console.log(ninjaOne)
+
+// let ninjaTwo :{
+//       name:string,
+//       age: number,
+//       beltColour:string,
+// }
+// ninjaTwo = {name:'shakil',age:20, beltColour:'red'}
+// console.log(ninjaTwo)
+// document.write('ninjaTwo');
+// let one:any[] = [];
+// one.push(20);
+// one.push('sorab');
+// one.push(true)
+// console.log(one)
+let right:{name:any,age:any};
+right ={name: 22, age:'habib'}
+console.log(right)
